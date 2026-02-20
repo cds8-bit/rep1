84a066e4bbd3b3f11efdf6d92e8539b1c53c351a
cds8-bit <cds8@discente.ifpe.edu.br>
2026-02-20 20:30:16 -0300

    Update __init__.py

---
+++ functions/__init__.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from libs.plot import plot_ts
+
+st.title('Stocks History')
+st.write('Lool the stocks values')
+
+ticker = st.sidebar.text_input('Choose the ticket: ', value= 'AAPL')
+
+fig = plot_ts(ticker)
+
+st.plotly_chart(fig)
